7faf3273c127ff7b6b052df3eb838b3f48532ed9
Nour05Nasr <nour.a.nasr05@gmail.com>
2025-12-03 07:20:46 +0200

    feat: add SideNav component

--- src/Components/Layout/SideNav.jsx
+++ src/Components/Layout/SideNav.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import icon1 from '../../Assets/icon1.svg';
+import icon2 from '../../Assets/icon2.svg';
+import icon3 from '../../Assets/icon3.svg';
+import icon4 from '../../Assets/icon4.svg';
+import icon5 from '../../Assets/icon5.svg';
+import icon6 from '../../Assets/icon6.svg';
+import icon7 from '../../Assets/icon7.svg';
 import logo from '../../Assets/logo.svg';
 import NavCta from '../Common/NavCta';
 import NavLi from '../Common/NavLi';
@@ -7,10 +13,18 @@ import './SideNav.css';
 
 const SideNav = () => {
     return ( <>
-    <div className=''>
+    <div className='side_nav'>
         <img src={logo} alt="NN Logo" />
+        <div className='inner_nav'>
         <NavLi title="Dshboard" img={icon1} />
-        {/* <NavCta cta ="Sign Out" /> */}
+        <NavLi title="Analytics" img={icon2} />
+        <NavLi title="Categories" img={icon3} />
+        <NavLi title="Projects" img={icon4} />
+        <NavLi title="Pages" img={icon5} />
+        <NavLi title="Inbox" img={icon6} />
+        <NavLi title="Profile" img={icon7} />
+        </div>
+        <NavCta cta ="Sign Out" />
     </div>
     </> );
 }
