70852a86c6d115c8b3a27366a9fee31438b1c50e
Nour05Nasr <nour.a.nasr05@gmail.com>
2025-12-03 09:32:24 +0200

    feat: add dasboard page

---
+++ src/Pages/Analytics.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import SideNav from '../Components/Layout/SideNav';
+import piechart from '../Assets/piechart.png';
+import sales from '../Assets/sales.png';
+import visits from '../Assets/visits.png';
+import analysis from '../Assets/visits.png';
+import status from '../Assets/visits.png';
+import gra1 from '../Assets/gra1.png';
+import gra2 from '../Assets/gra2.png';
+import gra3 from '../Assets/gra3.png';
+import gra4 from '../Assets/gra4.png';
+import './Dashboard.css';
+
+const Dashboard = () => {
+    return ( <>
+    <SideNav />
+    <div className='body_div'>
+        <div>
+    <img src={gra1} alt="" />
+    <img src={gra2} alt="" />
+    <img src={gra3} alt="" />
+    <img src={gra4} alt="" />
+        </div>
+        <div>
+    <img src={analysis} alt="" />
+    <img src={status} alt="" />
+        </div>
+    <img src={piechart} alt="" />
+    <img src={sales} alt="" />
+    <img src={visits} alt="" />
+    </div>
+    </> );
+}
+ 
+export default Dashboard;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import SideNav from '../Components/Layout/SideNav';
+import piechart from '../Assets/piechart.png';
+import sales from '../Assets/sales.png';
+import visits from '../Assets/visits.png';
 import './Dashboard.css';
 
 const Dashboard = () => {
     return ( <>
     <SideNav />
+    <div className='body_div'>
+    <img className='width_1' src={piechart} alt="" />
+    <img className='width_1' src={sales} alt="" />
+    <img className='width_1' src={visits} alt="" />
+    </div>
     </> );
 }
  
